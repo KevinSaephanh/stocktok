d81d006005c9f332280e909b27d4d55a75542ec8
Kevin <kevin.saephanh@gmail.com>
2023-03-15 23:20:00 -0500

    Update buttons

--- src/components/ui/Buttons/HamburgerButton.tsx
+++ src/components/ui/Buttons/HamburgerButton.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 
 type HamburgerButtonProps = {
   path: string;
@@ -8,13 +8,13 @@ export const HamburgerButton: React.FC<HamburgerButtonProps> = ({ path }) => {
   return (
     <>
       <svg
-        className="block h-8 w-8 rounded-full cursor-pointer hover:text-blue-500 dark:hover:text-slate-400"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        aria-hidden="true"
+        className='h-10 w-10 rounded-full cursor-pointer hover:text-teal-600 dark:hover:text-slate-400'
+        fill='none'
+        viewBox='0 0 24 24'
+        stroke='currentColor'
+        aria-hidden='true'
       >
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={path} />
+        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d={path} />
       </svg>
     </>
   );
