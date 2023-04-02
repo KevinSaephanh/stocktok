611a9a48d61f21ee99db66d1e38c2034496b00be
Kevin <kevin.saephanh@gmail.com>
2023-04-02 11:59:40 -0500

    Update visuals

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -6,46 +6,54 @@ import { ThemeButton } from '../../ui/Buttons/ThemeButton';
 import { NavLink } from './NavLink';
 import { HamburgerButton } from '../../ui/Buttons/HamburgerButton';
 import { SearchButton } from '../../ui/Buttons/SearchButton';
+import { SearchModal } from '../../ui/Modal/SearchModal';
 
 export const Navbar: React.FC = () => {
   const user = useAppSelector((state) => state.users.user);
   const [open, setOpen] = React.useState(false);
+  const [show, setShow] = React.useState(false);
+
+  const toggleShow = () => {
+    console.log('asfa');
+    setShow(true);
+  };
 
   return (
-    <nav className="border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
-      <div className="flex flex-wrap items-center justify-between">
+    <nav className='border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900'>
+      <div className='flex flex-wrap items-center justify-between'>
         {/* Logo */}
-        <Link href="/">
-          <a href="#">
-            <img src={Pepe.src} className="mr-3 h-9 ml-2" alt="Logo" />
+        <Link href='/'>
+          <a href='#'>
+            <img src={Pepe.src} className='mr-3 h-9 ml-2' alt='Logo' />
           </a>
         </Link>
 
         {/* Search and Theme Buttons */}
-        <div className="flex md:order-2">
-          <div className="flex flex-grow items-center">
+        <div className='flex md:order-2'>
+          <div className='flex flex-grow items-center'>
             <button
-              type="submit"
-              className="p-2 font-medium themed-text rounded-full hover:text-blue-500 hover:bg-slate-400 focus:ring-4 focus:outline-none focus:ring-slate-300 dark:hover:bg-slate-700 dark:focus:ring-slate-800"
+              type='submit'
+              className='p-2 font-medium themed-text rounded-full hover:text-blue-500 hover:bg-slate-400 focus:ring-4 focus:outline-none focus:ring-slate-300 dark:hover:bg-slate-700 dark:focus:ring-slate-800'
             >
               <SearchButton
-                classes="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                path="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                classes='w-6 h-6'
+                fill='none'
+                viewBox='0 0 24 24'
+                path='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+                onClick={toggleShow}
               />
-              <span className="sr-only">Search</span>
+              <span className='sr-only'>Search</span>
             </button>
             <ThemeButton />
           </div>
 
           {/* Hamburger menu */}
           <button
-            data-collapse-toggle="mobile-menu"
-            type="button"
-            className="inline-flex items-center p-2 ml-2 themed-text rounded-lg md:hidden"
-            aria-controls="mobile-menu-2"
-            aria-expanded="false"
+            data-collapse-toggle='mobile-menu'
+            type='button'
+            className='inline-flex items-center p-2 themed-text rounded-lg md:hidden'
+            aria-controls='mobile-menu-2'
+            aria-expanded='false'
             onClick={() => setOpen(!open)}
           >
             {!open ? (
@@ -62,9 +70,9 @@ export const Navbar: React.FC = () => {
             'justify-between items-center w-full md:flex md:w-auto md:order-1' +
             (open ? ' flex' : ' hidden')
           }
-          id="example-navbar-danger"
+          id='example-navbar-danger'
         >
-          <ul className="flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 rounded-lg border border-gray-100 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className='flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 rounded-lg border border-gray-100 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
             <NavLink href={'/about'} label={'About'} />
             <NavLink href={'/help'} label={'Help Center'} />
             {!user ? (
@@ -78,6 +86,8 @@ export const Navbar: React.FC = () => {
           </ul>
         </div>
       </div>
+
+      <SearchModal show={show} handleHideModal={() => setShow(false)} />
     </nav>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,39 +6,30 @@ import { SearchModal } from '../components/ui/Modal/SearchModal';
 const Home: NextPage = () => {
   const [show, setShow] = React.useState(false);
 
-  const handleShowModal = () => {
-    setShow(true);
-    console.log(show);
-  };
-
-  const handleHideModal = () => {
-    setShow(false);
-  };
-
   return (
-    <div className="container flex items-center justify-between mx-auto">
-      <div className="relative md:px-20 w-full sm:w-5/6 max-w-3xl">
-        <h1 className="text-6xl font-bold tracking-wide pb-8 md:pb-12">Title or Welcome Here</h1>
-        <div className="relative w-full">
-          <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+    <div className='container flex items-center justify-between mx-auto'>
+      <div className='relative md:px-20 w-full sm:w-5/6 max-w-3xl'>
+        <h1 className='text-6xl font-bold tracking-wide pb-8 md:pb-12'>Title or Welcome Here</h1>
+        <div className='relative w-full'>
+          <div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
             <SearchButton
-              classes="w-8 h-8"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              path="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              classes='w-8 h-8'
+              fill='currentColor'
+              viewBox='0 0 20 20'
+              path='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z'
             />
           </div>
           <input
-            type="text"
+            type='text'
             readOnly={true}
-            className="pl-14 p-2.5"
-            placeholder="Search tickers here"
-            onClick={handleShowModal}
+            className='pl-14 p-2.5'
+            placeholder='Search tickers here'
+            onClick={() => setShow(true)}
           />
         </div>
       </div>
 
-      {show ? <SearchModal handleHideModal={handleHideModal} /> : null}
+      <SearchModal show={show} handleHideModal={() => setShow(false)} />
     </div>
   );
 };
